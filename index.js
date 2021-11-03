c893f9d891fb1bd63b9cc0344efe80e6bbc5eac5
Dennis Karlsson <tougent@gmail.com>
2021-11-03 14:03:09 +0100

    init

---
+++ index.js
@@ -0,0 +1,43 @@
+const door = document.getElementById('doorClick');
+const playMusic = document.getElementById('musicBtn');
+
+const link = './sound/music.mp3';
+let audio = new Audio(link);
+audio.loop = true;
+
+let audioFiles = () => {
+    openDoor = new Audio('./sound/openDoor.mp3');
+    closeDoor = new Audio('./sound/closeDoor.mp3');
+    
+    openDoor.volume = 0.4;
+    closeDoor.volume = 0.4;
+
+    door.addEventListener('click', () => {
+        
+        if(door.checked){
+            setTimeout(() => {
+                openDoor.play();
+            }, 2500);
+
+            setTimeout(() => {
+                closeDoor.play();
+            }, 6000);
+        }
+    })
+}
+
+let bgMusic = paused => {
+    if(paused){
+        audio.pause();
+        return;
+    }
+
+    audio.play();
+}
+
+//audioFiles();
+
+playMusic.addEventListener('click', () => {
+    //bgMusic(false);
+})
+    
