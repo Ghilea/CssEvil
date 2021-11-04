7981f9866d0a52a7994d5c420d811cd08254f856
Dennis Karlsson <30557440+Ghilea@users.noreply.github.com>
2021-11-04 15:33:51 +0100

    Menu (#5)
    * New menu + a few fixes
    Added new menu and fixed a few things
    
    * menu fix

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const door = document.getElementById('doorClick');
-const playMusic = document.getElementById('musicBtn');
-
-const link = './sound/music.mp3';
-let audio = new Audio(link);
-audio.loop = true;
+const door = document.querySelector('#doorClick');
+const startGame = document.querySelector('#startGameBtn');
+const menuBtn = document.querySelectorAll('.menuBtn');
+const anyKey = document.querySelector('#anyBtnGameBtn');
 
 let audioFiles = () => {
     openDoor = new Audio('./sound/openDoor.mp3');
@@ -26,18 +24,57 @@ let audioFiles = () => {
     })
 }
 
+//music function
 let bgMusic = paused => {
+
     if(paused){
         audio.pause();
         return;
     }
+
+    const link = './sound/music.mp3';
+    let audio = new Audio(link);
+    audio.volume = 0.5;
+    audio.loop = true;
 
     audio.play();
 }
 
 audioFiles();
 
-playMusic.addEventListener('click', () => {
-    //bgMusic(false);
-})
+//start the game
+startGame.addEventListener('click', () => {
     
+    const link = './sound/startGame.mp3';
+    let audio = new Audio(link);
+    audio.volume = 0.5;
+    audio.play();
+    
+    setTimeout(() => {
+        bgMusic(false);
+    }, 2000);
+            
+})
+
+//hover over buttons
+menuBtn.forEach(element => {
+    element.addEventListener("mouseover", event => {
+
+        const link = './sound/button.mp3';
+        let audio = new Audio(link);
+        audio.volume = 0.5;
+        audio.play();
+
+        console.log("Mouse in");
+    });
+});
+
+//any key press
+window.addEventListener('keydown', function (e) {
+   let test = anyKey.checked = true;
+   console.log(test);
+});
+
+window.addEventListener('mousedown', function (e) {
+   let test = anyKey.checked = true;
+});
